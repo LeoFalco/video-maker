1f20400b8f9e6f1dac3f0fcdd471dd872ea4b5b9
Leonardo Jesus Falco <leonardo.falco@outlook.com>
2019-03-11 20:05:45 -0300

    iniciado index adiciona lib de leitura do terminal

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("teste");
+function start() {
+    const content = {};
+
+    content.searchTerm = askAndReturnSearchTerm();
+    content.prefix = askAndReturnPrefix();
+
+    function askAndReturnSearchTerm() {
+
+        return "termo";
+    }
+
+
+    function askAndReturnPrefix() {
+        return "prefix";
+    }
+
+    console.log(content);
+}
+
+start();
